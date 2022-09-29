82c044361d864d26fee0705a1b66b3c73f321816
‘Tarold’ <ltdecorer@gmail.com>
2022-09-29 17:16:38 +0300

    second try

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
 const Telegraf = require("telegraf").Telegraf;
 const os = require("node:os");
 const TOKEN =  "1495800116:AAHZWhlvTEvK91YMH47ta8yHAaKYFHH-zoM";
-
 const bot = new Telegraf(TOKEN);
-bot.start(ctx =>{                                                                                                                                                                                           
-});
 
-bot.hears(/Привіт/i, ctx=> {
-    ctx.reply("\u{1F600}");                                                                                                                                                                                                              
-});
+let serverData = {};
+let serverDate = "";
 
-bot.hears(/[A-Z]+/i, ctx=> 
-fetch("https://russianwarship.rip/api/v1/statistics/latest",
-{
-    method:"get",
-    headers:{'Content-Type': 'application/json'}
+function getDate(){
+    var date = new Date();
+
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = date.getFullYear();
+
+    date = yyyy + '-' + mm + '-' + dd;
+    return date;
 }
-)
-    .then(response => response.json())
-    .then (data =>
-        ctx.reply(ctx.message.text + ": " + data.data.stats[ctx.message.text]
-        + " + " + data.data.increase[ctx.message.text] + "\nТанків: "
-        + data["data"]["stats"]["tanks"] + " + "+data["data"]["increase"]["tanks"] )
-    )
-);
+
+async function getDataFromServer(forceFetch=false){
+    if (!forceFetch){
+        return 
+    }
+    return fetch("https://russianwarship.rip/api/v1/statistics/latest",{
+        method:"get",
+        headers:{'Content-Type': 'application/json'}
+    })
+        .then(response => response.json())
+        .then(data => {
+                serverData = data
+                serverDate = serverData.data.date
+            })
+            .catch((er) => {
+                console.log("Error ${er}")
+            })
+}
+
+bot.start(ctx =>{
+    ctx.replyWithHTML("Hihi", {
+        reply_markup:{
+            inline_keyboard: [
+                [{text : "Resource", url: "https://russianwarship.rip/"}],
+                [{text : "getAll", callback_data: "getAll"}],
+            ]
+        }
+    });
+});
+bot.action("getAll", ctx =>{
+    kindOfStatistic = "stats"
+    ctx.reply("PLease enter a kind of statistic ")
+})
+bot.hears(/Привіт/i, ctx=> {
+    ctx.reply("\u{1F600}");
+});
+
+bot.hears(/[A-Z]+/i,async ctx=> {
+    let message = ctx.message.text;
+    await getDataFromServer(serverDate != getDate())
+    ctx.reply(message +": " +serverData.data.stats[message])
+})
 
 bot.launch(); 
